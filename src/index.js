6ded4ae4b325f92a7963e0ad205a6404dab721e8
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-11-05 16:32:12 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import './index.html';
+import './index.scss';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+
+const termsSwiper = new Swiper('.terms__swiper', {
+	slidesPerView: 1,
+	preloadImages: true,
+	spaceBetween: 30,
+	slidesPerView: 1,
+	centeredSlides: true,
+	centeredSlidesBounds: true,
+	// centerInsufficientSlides: true,
+	direction: 'horizontal',
+	loop: false,
+	modules: [Navigation, Pagination],
+	pagination: {
+		el: '.terms__swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+    nextEl: '.terms__next',
+    prevEl: '.terms__prev',
+  },
+});
+
+const photoSwiper = new Swiper('.photo__swiper', {
+	slidesPerView: 1,
+	preloadImages: true,
+	spaceBetween: 30,
+	slidesPerView: 1,
+	centeredSlides: true,
+	direction: 'horizontal',
+	loop: false,
+	modules: [Navigation, Pagination],
+	pagination: {
+		el: '.photo__swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+    nextEl: '.photo__next',
+    prevEl: '.photo__prev',
+  },
+});
+
+const footerSwiper = new Swiper('.footer__swiper', {
+	slidesPerView: 1,
+	preloadImages: true,
+	spaceBetween: 30,
+	slidesPerView: 1,
+	centeredSlides: true,
+	direction: 'horizontal',
+	loop: false,
+	modules: [Navigation, Pagination],
+	pagination: {
+		el: '.footer__swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+    nextEl: '.footer__next',
+    prevEl: '.footer__prev',
+  },
+});
